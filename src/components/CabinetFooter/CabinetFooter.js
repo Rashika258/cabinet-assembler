1c1aa62bbeccad1cedbba8a67bc7a4c47a4d2501
Rashika258 <rashikasuresh5758@gmail.com>
2023-10-14 21:39:26 +0530

    footer changes

--- src/components/CabinetFooter/CabinetFooter.js
+++ src/components/CabinetFooter/CabinetFooter.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import './CabinetFooter.css';
+import React from "react";
+import "./CabinetFooter.css";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const CabinetFooter = () => {
-    const handleNavigation = () =>{
-
+  const location = useLocation();
+  const navigate = useNavigate();
+  const handleNavigation = () => {
+    if (location.pathname === "/") {
+      navigate("/parts-selection");
+    } else if (location.pathname === "/parts-selection") {
+      navigate("/assembly");
+    } else if (location.pathname === "/assembly") {
+      navigate("/final-product");
+    } else {
+      return;
     }
+    console.log("location", location);
+  };
   return (
-    <div className='cabinet__footer__wrap'>
-    <button className='cabinet__footer-btn' onClick={(e) => handleNavigation()}> Next</button>
-      
+    <div className="cabinet__footer__wrap">
+      <button
+        className="cabinet__footer-btn"
+        onClick={(e) => handleNavigation()}
+      >
+        Next
+      </button>
     </div>
   );
-}
+};
 
 export default CabinetFooter;
--- src/components/CabinetHomePage/CabinetHomePage.js
+++ src/components/CabinetHomePage/CabinetHomePage.js
@@ -6,12 +6,18 @@ import CabinetFooter from "../CabinetFooter/CabinetFooter";
 const CabinetHomePage = () => {
   return (
     <div className="cabinet__home__page">
-    <div className="cabinet__home__page-content">
-      <div className="home__page__description-wrap">
-        <h1 className="heading">Cabinet Assembler App</h1>
-        <p className="sub-text"></p>
-      </div>
-      <CompleteCabinet />
+      <div className="cabinet__home__page-content">
+        <div className="home__page__description-wrap">
+          <h1 className="heading">Cabinet Assembler</h1>
+          <p className="sub-text">
+            Are you ready to unleash your creativity and design your dream
+            cabinet? Our Cabinet Assembler website is your gateway to a world of
+            endless possibilities. Whether you're an aspiring fashionista, a toy
+            enthusiast, a perfume connoisseur, or someone who loves giving and
+            receiving gifts, our platform is designed for you.
+          </p>
+        </div>
+        <CompleteCabinet />
       </div>
       <CabinetFooter />
     </div>
