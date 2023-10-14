2b9625e722ba5fd9f20134ae9c9b62848c3859e1
Rashika258 <rashikasuresh5758@gmail.com>
2023-10-14 22:22:14 +0530

    Parts selection css changes

--- src/components/PartsSelection/PartsSelection.js
+++ src/components/PartsSelection/PartsSelection.js
@@ -2,50 +2,89 @@ import React from "react";
 import "./PartsSelection.css";
 import Carousel from "react-multi-carousel";
 import { cabinetPartsData } from "../../data";
-const responsive = {
-  desktop: {
-    breakpoint: {
-      max: 3000,
-      min: 1024
-    },
-    items: 3,
-    partialVisibilityGutter: 40
-  },
-  mobile: {
-    breakpoint: {
-      max: 464,
-      min: 0
-    },
-    items: 1,
-    partialVisibilityGutter: 30
-  },
-  tablet: {
-    breakpoint: {
-      max: 1024,
-      min: 464
-    },
-    items: 2,
-    partialVisibilityGutter: 30
-  }
-}
+import CabinetFooter from "../CabinetFooter/CabinetFooter";
+import leftArrow from "../../assets/left-arrow.png";
+import rightArrow from "../../assets/right-arrow.png";
 
-const PartsSelection = () => {
+export default function PartsSelection() {
+  const responsive = {
+    desktop: {
+      breakpoint: {
+        max: 3000,
+        min: 1024,
+      },
+      items: 3,
+      partialVisibilityGutter: 40,
+    },
+    mobile: {
+      breakpoint: {
+        max: 464,
+        min: 0,
+      },
+      items: 1,
+      partialVisibilityGutter: 30,
+    },
+    tablet: {
+      breakpoint: {
+        max: 1024,
+        min: 464,
+      },
+      items: 2,
+      partialVisibilityGutter: 30,
+    },
+  };
+
+  const CustomLeftArrow = ({ previous }) => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          position: "relative",
+        }}
+      >
+        <img
+          src={leftArrow}
+          classNameName="left__arrow"
+          alt="left"
+          onClick={previous}
+        />
+      </div>
+    );
+  };
+
+  const CustomRightArrow = ({ next }) => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          position: "relative",
+        }}
+      >
+        <img
+          src={rightArrow}
+          classNameName="right__arrow"
+          alt="right"
+          onClick={next}
+        />
+      </div>
+    );
+  };
+
   return (
-    // <div className="parts__selection__container">
-    //   <div class="ag-format-container">
-    //     <div class="ag-courses_box">
-          <Carousel
+    <div className="parts__selection__container">
+      <div className="parts__selection__carousel-wrap">
+        <Carousel
           additionalTransfrom={0}
           arrows
           autoPlaySpeed={3000}
           centerMode={false}
-          className=""
-          containerClass=""
-          dotListClass=""
+          classNameName=""
+          containerclassName=""
+          dotListclassName=""
           draggable
           focusOnSelect={false}
           infinite={false}
-          itemClass=""
+          itemclassName=""
           keyBoardControl
           minimumTouchDrag={80}
           pauseOnHover
@@ -58,38 +97,39 @@ const PartsSelection = () => {
           rtl={false}
           shouldResetAutoplay
           showDots={false}
-          sliderClass=""
+          sliderclassName=""
           slidesToSlide={1}
           swipeable
+          customLeftArrow={<CustomLeftArrow />}
+          customRightArrow={<CustomRightArrow />}
+        >
+          {cabinetPartsData &&
+            cabinetPartsData.length > 0 &&
+            cabinetPartsData.map((part, index) => {
+              return (
+                <div className="parts__carousel-card">
+                  <div className="parts__carousel-card-wrap">
+                    <div className="parts__carousel-card-bg"></div>
 
-          >
-            {cabinetPartsData &&
-              cabinetPartsData.length > 0 &&
-              cabinetPartsData.map((part, index) => {
-                return(   <div class="ag-courses_item">
-                <a href="#" class="ag-courses-item_link">
-                  <div class="ag-courses-item_bg"></div>
+                    <div className="parts__carousel-card-title">
+                      {part.title}
+                    </div>
 
-                  <div class="ag-courses-item_title">
-                   {part.title}
+                    <div className="parts__carousel-card-img-wrap">
+                      <img
+                        className="parts__carousel-card-img"
+                        src={part.imgURL}
+                        alt={part.title}
+                      />
+                    </div>
                   </div>
+                </div>
+              );
+            })}
+        </Carousel>
+      </div>
 
-                  <div class="ag-courses-item_date-box">
-                  <img src={part.imgURL} />
-                    Start:
-                    <span class="ag-courses-item_date">04.11.2022</span>
-                  </div>
-                </a>
-              </div>)
-             
-              })}
-          </Carousel>
-    //     </div>
-    //   </div>
-    // </div>
-
-
+      <CabinetFooter />
+    </div>
   );
-};
-
-export default PartsSelection;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as AssemblyScreen} from './AssemblyScreen/AssemblyScreen';
 export {default as PartsSelection} from './PartsSelection/PartsSelection';
 export {default as CabinetHomePage} from './CabinetHomePage/CabinetHomePage';
 export {default as ResultScreen} from './ResultScreen/ResultScreen';
+export {default as CabinetFooter} from './CabinetFooter/CabinetFooter';
